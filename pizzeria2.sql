ff791902efeb976d7e9f2fe759f797f021489738
amarouch <fettah.amarouch@free.fr>
2019-07-10 12:45:27 +0200

    nqdjn

---
+++ pizzeria2.sql
@@ -0,0 +1,192 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  localhost:3306
+-- Généré le :  Mer 10 Juillet 2019 à 12:43
+-- Version du serveur :  10.1.37-MariaDB-0+deb9u1
+-- Version de PHP :  7.0.33-0+deb9u3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `pizzeria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+CREATE TABLE `commande` (
+  `numero` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ingredient`
+--
+
+CREATE TABLE `ingredient` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prix` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ingredient_pizza`
+--
+
+CREATE TABLE `ingredient_pizza` (
+  `ingredient_id` int(11) NOT NULL,
+  `pizza_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pizza`
+--
+
+CREATE TABLE `pizza` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(45) NOT NULL,
+  `prix` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pizza_commande`
+--
+
+CREATE TABLE `pizza_commande` (
+  `pizza_id` int(11) NOT NULL,
+  `commande_numero` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  `nom` varchar(255) DEFAULT NULL,
+  `prenom` varchar(255) DEFAULT NULL,
+  `adresse` varchar(255) DEFAULT NULL,
+  `login` varchar(255) DEFAULT NULL,
+  `passwd` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD PRIMARY KEY (`numero`,`user_id`),
+  ADD KEY `fk_commande_user1_idx` (`user_id`);
+
+--
+-- Index pour la table `ingredient`
+--
+ALTER TABLE `ingredient`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `ingredient_pizza`
+--
+ALTER TABLE `ingredient_pizza`
+  ADD PRIMARY KEY (`ingredient_id`,`pizza_id`),
+  ADD KEY `fk_ingredient_has_pizza_pizza1_idx` (`pizza_id`),
+  ADD KEY `fk_ingredient_has_pizza_ingredient_idx` (`ingredient_id`);
+
+--
+-- Index pour la table `pizza`
+--
+ALTER TABLE `pizza`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `pizza_commande`
+--
+ALTER TABLE `pizza_commande`
+  ADD PRIMARY KEY (`pizza_id`,`commande_numero`),
+  ADD KEY `fk_pizza_has_commande_commande1_idx` (`commande_numero`),
+  ADD KEY `fk_pizza_has_commande_pizza1_idx` (`pizza_id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `commande`
+--
+ALTER TABLE `commande`
+  MODIFY `numero` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `ingredient`
+--
+ALTER TABLE `ingredient`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `pizza`
+--
+ALTER TABLE `pizza`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD CONSTRAINT `fk_commande_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `ingredient_pizza`
+--
+ALTER TABLE `ingredient_pizza`
+  ADD CONSTRAINT `fk_ingredient_has_pizza_ingredient` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredient` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_ingredient_has_pizza_pizza1` FOREIGN KEY (`pizza_id`) REFERENCES `pizza` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `pizza_commande`
+--
+ALTER TABLE `pizza_commande`
+  ADD CONSTRAINT `fk_pizza_has_commande_commande1` FOREIGN KEY (`commande_numero`) REFERENCES `commande` (`numero`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_pizza_has_commande_pizza1` FOREIGN KEY (`pizza_id`) REFERENCES `pizza` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
